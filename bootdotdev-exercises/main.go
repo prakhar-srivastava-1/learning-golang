83ee12b7dbf3cffc56f4af350ab8dbe91248376c
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2025-07-02 23:51:21 +0530

    solve two constants and add horizontal rules

--- bootdotdev-exercises/main.go
+++ bootdotdev-exercises/main.go
@@ -2,31 +2,46 @@ package main
 
 import "fmt"
 
+const HR = "==================================================================================="
+
 func main() {
 	fmt.Println("Running main()")
+	fmt.Println(HR)
 	// call the exercise functions here
 	// ex-1
-	fmt.Print("Exercise 1: ")
+	fmt.Println("Exercise 1")
 	basicVariables()
+	fmt.Println(HR)
 	// ex-2
-	fmt.Print("Exercise 2: ")
+	fmt.Println("Exercise 2")
 	fixBug()
+	fmt.Println(HR)
 	// ex-3
-	fmt.Print("Exercise 3: ")
+	fmt.Println("Exercise 3")
 	fixCompilationError()
+	fmt.Println(HR)
 	// ex-4
-	fmt.Print("Exercise 4: ")
+	fmt.Println("Exercise 4")
 	concatenateStrings()
+	fmt.Println(HR)
 	// ex-5
-	fmt.Print("Exercise 5: ")
+	fmt.Println("Exercise 5")
 	shortDeclaration()
+	fmt.Println(HR)
 	// ex-6
-	fmt.Print("Exercise 6: ")
+	fmt.Println("Exercise 6")
 	typeInference()
+	fmt.Println(HR)
 	// ex-7
-	fmt.Print("Exercise 7: ")
+	fmt.Println("Exercise 7")
 	sameLineDeclaration()
+	fmt.Println(HR)
 	// ex-8
-	fmt.Print("Exercise 8: ")
+	fmt.Println("Exercise 8")
 	typeSizes()
+	fmt.Println(HR)
+	// ex-9
+	fmt.Println("Exercise 9")
+	twoConstants()
+	fmt.Println(HR)
 }
